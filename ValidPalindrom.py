96f13b77b3274ebecb3374c12f646bcee896abc7
Gianluca Caregnato <gianluca.caregnato@studenti.unipd.it>
2024-11-13 18:43:35 +0100

    ValidPalindrom.py

--- ValidPalindrom.py
+++ ValidPalindrom.py
@@ -7,7 +7,7 @@ class ValidPalindrom:
                 a += 1
             while not s[- (1 + i + b)].isalpha():
                 b += 1
-            print(f"{s[i + a]}, {s[- (1 + i + b)]}, index i: {i}, a: {a}, b: {b}")
+
             if i + a > len(s) - 1 - i - b:
                 break
             if s[i + a] != s[- (1 + i + b)]:
